bfd69e85b3601651f3bcb75401a0660b6d040c12
thomas15v <tvanmellaerts@live.be>
2014-11-05 11:25:18 +0100

    We are playing Local, Game shouldn't add entities. In fact it shouldn't be doing anything.

--- src/main/java/edu/howest/breakout/Game/Game.java
+++ src/main/java/edu/howest/breakout/Game/Game.java
@@ -13,38 +13,15 @@ import java.util.Observable;
  * Future class in case we would implement multiplayer
  */
 public abstract class Game extends Observable implements Runnable {
-    private GameProperties gameProperties;
-    private List<Entity> entities;
-    private GameState gameState;
-    private Dimension dimension = new Dimension(500,500);
+    protected GameProperties gameProperties;
+    protected List<Entity> entities;
+    protected GameState gameState;
+    protected Dimension dimension = new Dimension(500,500);
 
     public Game(GameProperties properties){
         this.entities = new ArrayList<Entity>();
         this.gameProperties = properties;
-        entities.add(new EntityBall(10,10));
-        entities.add(new EntityBlock(10,10, Color.black, 20,20));
         gameState = GameState.Running;
-    }
-
-    @Override
-    public void run() {
-        while (gameState == GameState.Running) {
-            try {
-                tick();
-                Thread.sleep(50);
-            } catch (Exception e) {
-                e.printStackTrace();
-                gameState = GameState.Errored;
-            }
-        }
-        notifyObservers();
-    }
-
-    public void tick(){
-        for (Entity e : entities)
-            e.tick(this);
-        setChanged();
-        notifyObservers();
     }
 
     public List<Entity> getEntities() {
--- src/main/java/edu/howest/breakout/Game/LocalGame.java
+++ src/main/java/edu/howest/breakout/Game/LocalGame.java
@@ -1,5 +1,11 @@
 package edu.howest.breakout.game;
 
+import edu.howest.breakout.game.entity.Entity;
+import edu.howest.breakout.game.entity.EntityBall;
+import edu.howest.breakout.game.entity.EntityBlock;
+
+import java.awt.*;
+
 /**
  * Created by thomas on 04/11/2014.
  */
@@ -7,5 +13,28 @@ public class LocalGame extends Game {
 
     public LocalGame(GameProperties properties) {
         super(properties);
+        entities.add(new EntityBall(10,10));
+        entities.add(new EntityBlock(10,10, Color.black, 20,20));
+    }
+
+    @Override
+    public void run() {
+        while (gameState == GameState.Running) {
+            try {
+                tick();
+                Thread.sleep(50);
+            } catch (Exception e) {
+                e.printStackTrace();
+                gameState = GameState.Errored;
+            }
+        }
+        notifyObservers();
+    }
+
+    private void tick(){
+        for (Entity e : entities)
+            e.tick(this);
+        setChanged();
+        notifyObservers();
     }
 }
