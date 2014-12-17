6077eb3fb30ad02f70663d19cac7266d80d4842b
Nico Viaene <nicoviaene@hotmail.com>
2014-12-17 13:46:52 +0100

    Changed Angle calculation on entityball

--- src/main/java/edu/howest/breakout/game/entity/EntityBall.java
+++ src/main/java/edu/howest/breakout/game/entity/EntityBall.java
@@ -10,6 +10,7 @@ public class EntityBall extends Entity {
 
     private boolean lostBall = false;
     private Wall wall;
+    private int bereik = 90;
 
     public EntityBall(int x, int y){
         this(x, y, Wall.bottom);
@@ -22,7 +23,7 @@ public class EntityBall extends Entity {
         setWidth(20);
         setHeight(20);
         setSpeed(10);
-        setAngle(/*new Random().nextInt()*/ 45);
+        setAngle(/*new Random().nextInt()*/ 160);
     }
 
     @Override
@@ -45,32 +46,49 @@ public class EntityBall extends Entity {
         for (Entity e : game.getEntities()) {
             if (e instanceof EntityBlock) {
                 boolean hadbounce = true;
-                if (collide(e, getX() + getStraalX(), getY() + getHeight())) {
+                if (collide(e, getX() + getStraalX(), getY() + getHeight())) { //collision met bovenkant
+                    if (e instanceof EntityPad){
+                        changeAngle(e);
+                    }
                     bounceY();
                     setY(e.getY() - getHeight());
 
-                }else if (collide(e, getX() + getStraalX() , getY())) {
+                }else if (collide(e, getX() + getStraalX() , getY())) { //collision met onderkant
+                    if (e instanceof EntityPad){
+                        changeAngle(e);
+                    }
                     bounceY();
                     setY(e.getY() + e.getHeight());
                 }
-                else if (collide(e, getX(), getY() + getStraalY())) {
+                else if (collide(e, getX(), getY() + getStraalY())) { //collision met rechterkant
                     bounceX();
                     setX(e.getX() + e.getWidth());
                 }
-                else if (collide(e, getX() + getWidth(), getY() + getStraalY())) {
+                else if (collide(e, getX() + getWidth(), getY() + getStraalY())) { //collision met linkerkant
                     bounceX();
                     setX(e.getX() - getHeight());
                 }else
                 {
                     hadbounce = false;
                 }
-                if (e instanceof EntityBlock && hadbounce)
+                if (hadbounce)
                     ((EntityBlock) e).DoAction(game, this);
 
 
             }
         }
     }
+
+    private void changeAngle(Entity e){ //op basis van positie op pad
+        double locatiePad = ((getX() - e.getX() + getWidth()/2)/e.getWidth())*bereik;
+        //int oldAngle = getAngle();
+
+
+        int newAngle = ((int)locatiePad)-bereik/2;
+        setAngle(newAngle);
+
+    }
+
 
     private void bounceX(){
         setAngle(getAngle() * -1);
